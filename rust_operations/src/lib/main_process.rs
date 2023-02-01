be4f5dcb1fce0200654e4d75486a7face1c79cae
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-02-01 13:23:45 +0300

    data operations on local

--- rust_operations/src/lib/main_process.rs
+++ rust_operations/src/lib/main_process.rs
@@ -1,6 +1,26 @@
+use csv::Error;
 
-    pub fn tolga(){
+pub fn tolga(){
         println!("MAIN PROCESS");
     }
+
+pub fn main() -> Result<(), Error> {
+    let csv = "year,make,model,description
+        1948,Porsche,356,Luxury sports car
+        1967,Ford,Mustang fastback 1967,American car";
+
+    let mut reader = csv::Reader::from_reader(csv.as_bytes());
+    for record in reader.records() {
+        let record = record?;
+        println!(
+            "{}, {},{},{}",
+            &record[0],
+            &record[1],
+            &record[2],
+            &record[3]
+        )
+    }
+    Ok(())
+}
 
 
--- rust_operations/src/main.rs
+++ rust_operations/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
         username: String::from("Tolga")
     };
     println!("{:?}", tolga);
+    println!("{}" , String::from(lib::main_process::main()));
     lib::main_process::tolga();
     println!("Hello, world!");
 }
